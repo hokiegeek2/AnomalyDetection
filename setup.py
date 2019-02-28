c58243dbee2ac618dcc75d1b8419044abad81f59
hokiegeek2 <hokiegeek2@users.noreply.github.com>
2019-02-28 09:54:05 -0500

    Fixed per integration testing

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     packages=["anomaly_detection"],
     version="0.0.3",
     description="A pure python implementation of https://github.com/twitter/AnomalyDetection",
-    install_requires=['pandas','dask','toolz','cloudpickle','statsmodels','asn1crypto']
+    install_requires=['scipy','pandas','dask','toolz','cloudpickle','statsmodels','asn1crypto']
 )
